339db7e73c501ec099fc61257df25c0e63bce636
degrandelucas <lucasdegrande15@gmail.com>
2025-02-12 05:37:06 -0300

    codigo formatado

--- AnoBissexto.php
+++ AnoBissexto.php
@@ -3,8 +3,7 @@
 $ano = 2025;
 
 if (($ano % 4 == 0 && $ano % 100 != 0) || $ano % 400 == 0) {
-    echo "O ano é bissexto";
-}
-    else {
-    echo "O ano não é bissexto"; 
+    echo 'O ano é bissexto';
+} else {
+    echo 'O ano não é bissexto';
 }
